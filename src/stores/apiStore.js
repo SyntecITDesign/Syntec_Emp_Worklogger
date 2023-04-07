3d7a319a7414b60e1786e9d19f5542aa56c716b9
pearlwu0310@gmail.com <pearlwu0310@gmail.com>
2023-04-07 16:39:01 +0800

    apiUrl改為正式機連結

--- src/stores/apiStore.js
+++ src/stores/apiStore.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 export const useApiStore = defineStore('apiStore', () => {
-    const apiUrl = "https://localhost:44303/SyntecIT/api/v1";
+    const apiUrl = "https://www.syntecclub.com:9392/SyntecIT/api/v1";
 
     return { apiUrl }
 })
