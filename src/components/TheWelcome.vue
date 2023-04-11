d15cd00bb6fa99a2d43fa535700398273cada6ee
peiyuanwu10190441 <peiyuan.wu@syntecclub.com.tw>
2023-04-11 16:15:24 +0800

    修改歡迎頁面

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+import { NSpace } from "naive-ui";
+</script>
 
 <template>
-  <h1>歡迎使用JIRA報工系統</h1>
+  <div class="welcome">
+    <h1>歡迎使用JIRA報工系統</h1>
+  </div>
 </template>
+
+<style scoped>
+.welcome {
+  margin: 10% 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2.5rem;
+}
+</style>
