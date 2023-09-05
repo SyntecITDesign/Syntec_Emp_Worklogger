8696b2b5d661df1e2988ca9d8e771f69abaa6d90
SyntecITDesign <129140919+SyntecITDesign@users.noreply.github.com>
2023-09-05 11:28:53 +0800

    Revert "Modify layout py"

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { storeToRefs } from "pinia";
 import {
   NSpace,
   NInput,
@@ -10,7 +11,6 @@ import {
 } from "naive-ui";
 import { GlassesOutline } from "@vicons/ionicons5";
 import { onBeforeMount } from "vue";
-import { storeToRefs } from "pinia";
 import { useLogInStore } from "../stores/logInStore.js";
 import { useSiderStore } from "../stores/siderStore.js";
 const siderStore = useSiderStore();
@@ -30,42 +30,49 @@ onBeforeMount(() => {
 
 <template>
   <n-scrollbar style="max-height: 100vmin">
-    <n-space vertical
+    <n-space
+      vertical
       class="NSpace"
       @keyup.enter="handleValidateButtonClick"
-      @blur="handleValidateButtonClick"><img class="logo"
-        v-if="wholeTheme"
-        src="../assets/SyntecLogo.png" /><img class="logo"
+      @blur="handleValidateButtonClick"
+      ><img class="logo" v-if="wholeTheme" src="../assets/SyntecLogo.png" /><img
+        class="logo"
         v-else
-        src="../assets/SyntecLogoDark.png" /> 報工系統登入頁面 <n-form ref="formRef"
-        :model="model"
-        :rules="rules">
-        <n-form-item path="Username"
-          label="JIRA登入帳號">
-          <n-input class="NInput"
+        src="../assets/SyntecLogoDark.png"
+      />
+      報工系統登入頁面
+      <n-form ref="formRef" :model="model" :rules="rules">
+        <n-form-item path="Username" label="JIRA登入帳號">
+          <n-input
+            class="NInput"
             type="text"
             size="large"
             v-model:value="model.Username"
-            placeholder="帳號" />
+            placeholder="帳號"
+          />
         </n-form-item>
-        <n-form-item path="Password"
-          label="JIRA登入密碼">
-          <n-input class="NInput"
+        <n-form-item path="Password" label="JIRA登入密碼">
+          <n-input
+            class="NInput"
             type="password"
             size="large"
             show-password-on="mousedown"
             v-model:value="model.Password"
-            placeholder="密碼" />
+            placeholder="密碼"
+          />
         </n-form-item>
       </n-form>
+
       <template #password-visible-icon>
-        <n-icon :size="16"
-          :component="GlassesOutline" />
+        <n-icon :size="16" :component="GlassesOutline" />
       </template>
-      <n-button size="large"
+      <n-button
+        size="large"
         @click="handleValidateButtonClick"
         :loading="access.isChecking"
-        :disabled="access.isChecking">登入</n-button>
+        :disabled="access.isChecking"
+        >登入</n-button
+      >
     </n-space>
   </n-scrollbar>
 </template>
@@ -77,13 +84,11 @@ onBeforeMount(() => {
   align-items: center;
   font-size: 50px;
 }
-
 .logo {
   width: 95%;
   margin-top: 5%;
   margin-left: 5%;
 }
-
 .NInput {
   width: 600px;
   font-size: 30px;
